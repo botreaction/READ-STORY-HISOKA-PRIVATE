728061ff90c4d17d3464a41c46fc7a9ed2a4631b
wily <103911498+botreaction@users.noreply.github.com>
2024-11-26 12:15:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
 import path from 'path';
 import { spawn } from 'child_process';
 import { watchFile, unwatchFile } from 'fs';
